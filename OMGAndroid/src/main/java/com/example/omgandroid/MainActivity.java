e035642fe6b40005b5996acee5483e074da41334
Matt Luedke <mluedke2@gmail.com>
2013-10-06 23:19:41 -0700

    add share button

--- OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
+++ OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.omgandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,8 +23,9 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
     Button mainButton;
     EditText mainEditText;
     ListView mainListView;
-    ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> nameList = new ArrayList<String>();
+    ArrayAdapter<String> mArrayAdapter;
+    ArrayList<String> mNameList = new ArrayList<String>();
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,7 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
 
         // Access the TextView defined in layout XML and set its text
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        mainTextView.setText("Set in Java");
+        //mainTextView.setText("Set in Java");
 
         // Access the Button defined in layout XML and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
@@ -43,24 +47,48 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         mainListView = (ListView) findViewById(R.id.main_listview);
 
         // Create an ArrayAdapter for the ListView
-        arrayAdapter = new ArrayAdapter<String>(this,
+        mArrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                nameList);
+                mNameList);
 
         // Set the ListView to use the ArrayAdapter
-        mainListView.setAdapter(arrayAdapter);
+        mainListView.setAdapter(mArrayAdapter);
 
         // Set this activity to react to list items being pressed
         mainListView.setOnItemClickListener(this);
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider) shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share our content
+        setShareIntent();
+
         return true;
+    }
+
+    private void setShareIntent() {
+
+        // create an Intent with the contents of the TextView
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+
+        // Make sure the provider knows it should work with that Intent
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 
     @Override public void onClick(View v) {
@@ -69,17 +97,20 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
 
         // 2. Take what was typed into the EditText and use in TextView
         mainTextView.setText(mainEditText.getText().toString()
-              + " is an Android developer!");
+                + " is an Android developer!");
 
         // 3. Also add that value to the list shown in the ListView
-        nameList.add(mainEditText.getText().toString());
-        arrayAdapter.notifyDataSetChanged();
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+
+        // 4. The text we'd like to share has changed, and we need to update
+        setShareIntent();
 
     }
 
     @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         // Log the item's position and contents to the console in Debug
-        Log.d("omg android", position + ": " + nameList.get(position));
+        Log.d("omg android", position + ": " + mNameList.get(position));
 
     }
 }
