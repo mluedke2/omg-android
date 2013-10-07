33fe66f9db1839725cf9a8ccfc485a0eb6d196e5
Matt Luedke <mluedke2@gmail.com>
2013-10-06 17:48:33 -0700

    textview set in java

--- OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
+++ OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
@@ -3,13 +3,21 @@ package com.example.omgandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java");
+
+
     }
 
 
