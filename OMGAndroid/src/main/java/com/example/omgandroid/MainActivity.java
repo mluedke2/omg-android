1b71150486b4e2143d2eb119277736f536a4466a
Matt Luedke <mluedke2@gmail.com>
2013-10-06 18:15:36 -0700

    created listview and adapter, can add names to list

--- OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
+++ OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.omgandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> nameList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // Access the EditText defined in layout XML
         mainEditText = (EditText) findViewById(R.id.main_edittext);
 
+        // Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                nameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(arrayAdapter);
+
     }
 
 
@@ -41,12 +59,16 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     @Override public void onClick(View v) {
-      // Originally used to test the Button
-      //  mainTextView.setText("Button pressed!");
+        // 1. Test the Button
+        //  mainTextView.setText("Button pressed!");
 
-        // take what was typed into the EditText and use in TextView
-        mainTextView.setText(mainEditText.getText()
-                + " is an Android developer!");
+        // 2. Take what was typed into the EditText and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+              + " is an Android developer!");
+
+        // 3. Also add that value to the list shown in the ListView
+        nameList.add(mainEditText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
 
     }
 }
