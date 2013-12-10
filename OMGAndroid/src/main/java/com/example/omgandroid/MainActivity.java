8fcb701a6dbd99a75230985d169a757d6bbc2710
Matt Luedke <mluedke2@gmail.com>
2013-12-09 23:51:44 -0800

    updates to res

--- OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
+++ OMGAndroid/src/main/java/com/example/omgandroid/MainActivity.java
@@ -21,37 +21,40 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class MainActivity extends Activity
+        implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
 
-    private static final String PREFS = "prefs";
-    private static final String PREF_NAME = "name";
-
-    TextView mainTextView;
-    Button mainButton;
-    EditText mainEditText;
-    ListView mainListView;
+    TextView mainTextView; // 1
+    Button mainButton; // 2
+    EditText mainEditText; // 3
+    ListView mainListView; // 4
     ArrayAdapter<String> mArrayAdapter;
     ArrayList<String> mNameList = new ArrayList<String>();
-    ShareActionProvider mShareActionProvider;
-    SharedPreferences mSharedPreferences;
+    ShareActionProvider mShareActionProvider; // 6
+    private static final String PREFS = "prefs"; // 7
+    private static final String PREF_NAME = "name"; // 7
+    SharedPreferences mSharedPreferences; // 7
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Access the TextView defined in layout XML and set its text
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
         mainTextView = (TextView) findViewById(R.id.main_textview);
-        //mainTextView.setText("Set in Java");
+        mainTextView.setText("Set in Java!");
 
-        // Access the Button defined in layout XML and listen for it here
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
 
-        // Access the EditText defined in layout XML
+        // 3. Access the EditText defined in layout XML
         mainEditText = (EditText) findViewById(R.id.main_edittext);
 
-        // Access the ListView
+        // 4. Access the ListView
         mainListView = (ListView) findViewById(R.id.main_listview);
 
         // Create an ArrayAdapter for the ListView
@@ -62,19 +65,21 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         // Set the ListView to use the ArrayAdapter
         mainListView.setAdapter(mArrayAdapter);
 
-        // Set this activity to react to list items being pressed
+        // 5. Set this activity to react to list items being pressed
         mainListView.setOnItemClickListener(this);
 
-        // Greet the user, or ask for their name if new
+        // 7. Greet the user, or ask for their name if new
         displayWelcome();
     }
 
     public void displayWelcome() {
 
         // Access the device's key-value storage
-        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        mSharedPreferences
+                = getSharedPreferences(PREFS, MODE_PRIVATE);
 
-        // Read the user's name, or an empty string if nothing found
+        // Read the user's name,
+        // or an empty string if nothing found
         String name = mSharedPreferences.getString(PREF_NAME, "");
 
         if (name.length() > 0) {
@@ -87,7 +92,8 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         } else {
 
             // otherwise, show a dialog to ask for their name
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            AlertDialog.Builder alert
+                    = new AlertDialog.Builder(this);
             alert.setTitle("Hello!");
             alert.setMessage("What is your name?");
 
@@ -96,50 +102,86 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
             alert.setView(input);
 
             // Make an "OK" button to save the name
-            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
+            alert.setPositiveButton("OK",
+                    new DialogInterface.OnClickListener() {
 
-                    // Grab the EditText's input
-                    String inputName = input.getText().toString();
+                        public void onClick(DialogInterface dialog,
+                                            int whichButton) {
 
-                    // Put it into memory (don't forget to commit!)
-                    SharedPreferences.Editor e = mSharedPreferences.edit();
-                    e.putString(PREF_NAME, inputName);
-                    e.commit();
+                            // Grab the EditText's input
+                            String inputName = input.getText().toString();
 
-                    // Welcome the new user
-                    Toast.makeText(getApplicationContext(),
-                            "Welcome, " + inputName + "!",
-                            Toast.LENGTH_LONG)
-                            .show();
-                }
-            });
+                            // Put it into memory (don't forget to commit!)
+                            SharedPreferences.Editor e =
+                                    mSharedPreferences.edit();
+                            e.putString(PREF_NAME, inputName);
+                            e.commit();
 
-            // Make a "Cancel" button that simply dismisses the alert
-            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                }
-            });
+                            // Welcome the new user
+                            Toast.makeText(getApplicationContext(),
+                                    "Welcome, " + inputName + "!",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+            // Make a "Cancel" button
+            // that simply dismisses the alert
+            alert.setNegativeButton("Cancel",
+                    new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog,
+                                            int whichButton) {}
+                    });
 
             alert.show();
         }
     }
 
+    @Override public void onClick(View v) {
+
+        // 3. Take what was typed into the EditText
+        // and use in TextView
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+
+        // 4. Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+
+        // 6. The text you'd like to share has changed,
+        // and you need to update
+        setShareIntent();
+    }
+
+    @Override public void onItemClick(AdapterView<?> parent,
+                                      View view, int position, long id) {
+
+        // 5. Log the item's position and contents
+        // to the console in Debug
+        Log.d("omg android", position
+                + ": "
+                + mNameList.get(position));
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
 
         // Access the Share Item defined in menu XML
         MenuItem shareItem = menu.findItem(R.id.menu_item_share);
 
-        // Access the object responsible for putting together the sharing submenu
+        // Access the object responsible for
+        // putting together the sharing submenu
         if (shareItem != null) {
-            mShareActionProvider = (ShareActionProvider) shareItem.getActionProvider();
+            mShareActionProvider = (ShareActionProvider)
+                    shareItem.getActionProvider();
         }
 
-        // Create an Intent to share our content
+        // Create an Intent to share your content
         setShareIntent();
 
         return true;
@@ -150,31 +192,13 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
         // create an Intent with the contents of the TextView
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Android Development");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, mainTextView.getText());
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,
+                "Android Development");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mainTextView.getText());
 
-        // Make sure the provider knows it should work with that Intent
+        // Make sure the provider knows
+        // it should work with that Intent
         mShareActionProvider.setShareIntent(shareIntent);
-    }
-
-    @Override public void onClick(View v) {
-        // 1. Test the Button
-        //  mainTextView.setText("Button pressed!");
-
-        // 2. Take what was typed into the EditText and use in TextView
-        mainTextView.setText(mainEditText.getText().toString()
-                + " is learning Android development!");
-
-        // 3. Also add that value to the list shown in the ListView
-        mNameList.add(mainEditText.getText().toString());
-        mArrayAdapter.notifyDataSetChanged();
-
-        // 4. The text we'd like to share has changed, and we need to update
-        setShareIntent();
-    }
-
-    @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // Log the item's position and contents to the console in Debug
-        Log.d("omg android", position + ": " + mNameList.get(position));
     }
 }
